cdb86ce3c235e4a4945eb168363fc23215ee1d1a
Ubuntu <ubuntu@ip-172-31-24-92.ec2.internal>
2024-10-25 17:36:46 +0000

    Updated Code

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,18 +1,18 @@
-import java.util.logging.Logger;
-
+import java.util.Scanner;
 class Dec2Hex {
+
     public static int Arg1;
-    private static final Logger logger = Logger.getLogger(Dec2Hex.class.getName());
 
     public static void main(String args[]) {
         if (args.length == 0) {
-            logger.severe("Error: Please enter a Number.");
+            System.out.println("Error: Please enter a Number");
             return;
         }
+
         try {
             Arg1 = Integer.parseInt(args[0]);
         } catch (NumberFormatException e) {
-            logger.severe("Error: The input provided is not a valid integer. Please enter a valid integer.");
+            System.out.println("Error: The input provided is not a valid integer. Please enter a valid integer.");
             return;
         }
 
@@ -21,7 +21,7 @@ class Dec2Hex {
         num = Arg1;
         String hexadecimal = "";
 
-        logger.info("Converting the Decimal Value " + num + " to Hex...");
+        System.out.println("Converting the Decimal Value " + num + " to Hex...");
 
         while (num != 0) {
             rem = num % 16;
@@ -29,7 +29,7 @@ class Dec2Hex {
             num = num / 16;
         }
 
-        logger.info("Hexadecimal representation is: " + hexadecimal);
+        System.out.println("Hexadecimal representation is: " + hexadecimal);
     }
 }
 
