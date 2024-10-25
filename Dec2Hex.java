3ebd98f73cc4ae7bd64d665f9d32c916c770bb8b
Ubuntu <ubuntu@ip-172-31-24-92.ec2.internal>
2024-10-25 17:16:09 +0000

    Updated Code

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -4,14 +4,18 @@ class Dec2Hex {
     public static int Arg1;
 
     public static void main(String args[]) {
+	//if there is no input
         if (args.length == 0) {
             System.out.println("Error: Please enter a Number.");
             return;
         }
-
+		//if the input isnt a  valid number
         try {
             Arg1 = Integer.parseInt(args[0]);
-        } catch (NumberFormatException e) {
+
+        }
+	//catch the error and display a message
+		 catch (NumberFormatException e) {
             System.out.println("Error: The input provided isn't a valid number, Please enter a valid Number.");
             return;
         }
