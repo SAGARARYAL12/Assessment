c96ed912ecb9e2a5faa18c3d9e48a6acb9c54705
Ubuntu <ubuntu@ip-172-31-24-92.ec2.internal>
2024-10-25 17:49:09 +0000

    Updated code 2

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -21,7 +21,9 @@ class Dec2Hex {
         num = Arg1;
         String hexadecimal = "";
 
-        logger.info(String.format("Converting the Decimal Value %d to Hex...", num));
+        if (logger.isLoggable(java.util.logging.Level.INFO)) {
+            logger.info(String.format("Converting the Decimal Value %d to Hex...", num));
+        }
 
         while (num != 0) {
             rem = num % 16;
@@ -29,6 +31,9 @@ class Dec2Hex {
             num = num / 16;
         }
 
-        logger.info(String.format("Hexadecimal representation is: %s", hexadecimal));
+        if (logger.isLoggable(java.util.logging.Level.INFO)) {
+            logger.info(String.format("Hexadecimal representation is: %s", hexadecimal));
+        }
     }
 }
+
