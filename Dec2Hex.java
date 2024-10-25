b6b50d29ab406515e2a874cdc97f259f34fdc06e
Ubuntu <ubuntu@ip-172-31-24-92.ec2.internal>
2024-10-25 18:11:04 +0000

    Updated code 2

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,44 +1,46 @@
 import java.util.logging.Logger;
 
-class Dec2Hex {
+class Dec2Hex 
+{
     public static int Arg1;
     private static final Logger logger = Logger.getLogger(Dec2Hex.class.getName());
 
-    public static void main(String args[]) {
-        if (args.length == 0) {
+    public static void main(String args[]) 
+    {
+        if (args.length == 0) 
+	{
             logger.severe("Error: Please enter a Number.");
             return;
         }
-        
-        if (!isInteger(args[0])) {
+        try 
+	{
+            Arg1 = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
             logger.severe("Error: The input provided is not a valid integer. Please enter a valid integer.");
             return;
         }
 
-        Arg1 = Integer.parseInt(args[0]);
-        String hexadecimal = convertToHex(Arg1);
-        logger.info(String.format("Hexadecimal representation is: %s", hexadecimal));
-    }
-
-    public static String convertToHex(int num) {
         char ch[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-        StringBuilder hexadecimal = new StringBuilder();
+        int rem, num;
+        num = Arg1;
+        String hexadecimal = "";
 
-        while (num != 0) {
-            int rem = num % 16;
-            hexadecimal.insert(0, ch[rem]);
+        if (logger.isLoggable(java.util.logging.Level.INFO)) 
+	{
+            logger.info(String.format("Converting the Decimal Value %d to Hex...", num));
+        }
+
+        while (num != 0) 
+	{
+            rem = num % 16;
+            hexadecimal = ch[rem] + hexadecimal;
             num = num / 16;
         }
 
-        return hexadecimal.toString();
-    }
-
-    private static boolean isInteger(String str) {
-        try {
-            Integer.parseInt(str);
-            return true;
-        } catch (NumberFormatException e) {
-            return false;
+        if (logger.isLoggable(java.util.logging.Level.INFO)) 
+	{
+            logger.info(String.format("Hexadecimal representation is: %s", hexadecimal));
         }
     }
 }
+
